e0c33f5cba1f0fd144e18142fc7bbd5df12544e6
Ana Beatriz Alves da Costa <anabeatiz022@gmail.com>
2019-08-23 13:39:45 -0300

    Correção de ladda button no botão salvar

---
+++ produto.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+console.warn("olá mundo")
+
+let meuForm = $("#formProduto")
+console.log(meuForm)
+
+meuForm.on("submit", function(event){
+    event.preventDefault()
+    console.log("clicou")
+    prepararBotao()
+})
+
+    function prepararBotao(){
+    let botao = $("button#btnSalvar")
+    let ladda = Ladda.create(botao[0])
+    ladda.start()
+    }
+
+    function clicou(){
+        prepararBotao()
+    }
+}
